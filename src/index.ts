3cb55c0232bc706c7a735231e1ef8087049541ba
VAKozlov <vakozlov@sensebank.com.ua>
2026-09-24 15:04:41 +0300

    DEV-01: test app

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const app =
         new Size(640, 480),
         new ScaleStrategyFitCanvasToScreen()
     );
-app.runScene(new Test2Scene(app));
+app.runScene(new MainScene(app));
 
 const fpsElem = document.querySelector("#fps")!;
 fpsElem.textContent = `-`;
